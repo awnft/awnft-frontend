9542952ac06cb64a92f60c4f3b0d68b727c6fd7f
NGUYEN ANH TUAN <tuannapci@gmail.com>
2022-08-26 15:29:41 +0700

    Add routes

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch() {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h4>Page not found.</h4>;
+    }
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
---
+++ src/containers/Home/nftsList.js
@@ -0,0 +1,108 @@
+
+import StandardShovel from '../../images/StandardShovel.png';
+import StandardDrill from '../../images/StandardDrill.png';
+import StandardCapacitor from '../../images/StandardCapacitor.png';
+import RD9000Excavator from '../../images/RD9000Excavator.png';
+import QuarkSeparator from '../../images/QuarkSeparator.png';
+import ProcessingRing from '../../images/ProcessingRing.png';
+import PowerExtractor from '../../images/PowerExtractor.png';
+import LargeExplosive from '../../images/LargeExplosive.png';
+import LargeCapacitor from '../../images/LargeCapacitor.png';
+import InfusedExtractor from '../../images/InfusedExtractor.png';
+import GlavorDisc from '../../images/GlavorDisc.png';
+import GasriggedExtractor from '../../images/GasriggedExtractor.png';
+import DraxosAxe from '../../images/DraxosAxe.png';
+import CausianAttractor from '../../images/CausianAttractor.png';
+import BasicTriliumDetector from '../../images/BasicTriliumDetector.png';
+import BarrelDigger from '../../images/BarrelDigger.png';
+import ArtunianShovel from '../../images/ArtunianShovel.png';
+
+const list = [
+		{
+			name: 'Standard Shovel',
+			symbol: 'StandardShovel',
+			image: StandardShovel
+		},
+		{
+			name: 'Standard Drill',
+			symbol: 'StandardDrill',
+			image: StandardDrill
+		},
+		{
+			name: 'Standard Capacitor',
+			symbol: 'StandardCapacitor',
+			image: StandardCapacitor
+		},
+		{
+			name: 'RD9000 Excavator',
+			symbol: 'RD9000Excavator',
+			image: RD9000Excavator
+		},
+		{
+			name: 'Quark Separator',
+			symbol: 'QuarkSeparator',
+			image: QuarkSeparator
+		},
+		{
+			name: 'Processing Ring',
+			symbol: 'ProcessingRing',
+			image: ProcessingRing
+		},
+		{
+			name: 'Power Extractor',
+			symbol: 'PowerExtractor',
+			image: PowerExtractor
+		},
+		{
+			name: 'Large Explosive',
+			symbol: 'LargeExplosive',
+			image: LargeExplosive
+		},
+		{
+			name: 'Large Capacitor',
+			symbol: 'LargeCapacitor',
+			image: LargeCapacitor
+		},
+		{
+			name: 'Infused Extractor',
+			symbol: 'InfusedExtractor',
+			image: InfusedExtractor
+		},
+		{
+			name: 'Glavor Disc',
+			symbol: 'GlavorDisc',
+			image: GlavorDisc
+		},
+		{
+			name: 'Gasrigged Extractor',
+			symbol: 'GasriggedExtractor',
+			image: GasriggedExtractor
+		},
+		{
+			name: 'Draxos Axe',
+			symbol: 'DraxosAxe',
+			image: DraxosAxe
+		},
+		{
+			name: 'Causian Attractor',
+			symbol: 'CausianAttractor',
+			image: CausianAttractor
+		},
+		{
+			name: 'Basic Trilium Detector',
+			symbol: 'BasicTriliumDetector',
+			image: BasicTriliumDetector
+		},
+		{
+			name: 'Barrel Digger',
+			symbol: 'BarrelDigger',
+			image: BarrelDigger
+		},
+		{
+			name: 'Artunian Shovel',
+			symbol: 'ArtunianShovel',
+			image: ArtunianShovel
+		},
+	];
+
+export default list;
